af5fd2f14b5c13a90a7fc95dd07279d250089ced
morayod <morayodav@gmail.com>
2021-10-28 19:54:49 +0100

    ascii art exercise

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git.learn.01founders.co/morayod/ascii-art.git
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func read_file(s string) []string {
+	// read in file
+	content, err := ioutil.ReadFile(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// replace newlines with space and newline and split on whitespaces
+	text := strings.Replace(string(content), "\n", " %", -1)
+	words := strings.FieldsFunc(text, func(r rune) bool { return strings.ContainsRune("%", r) })
+	return words
+}
+
+func ascii_art() {
+
+	art := read_file("ascii-art")
+	letters := []rune(os.Args[1])
+	for j := 0; j < 8; j++ {
+		for i, letter := range letters {
+			fmt.Print(art[((int(letter)-32)*9)+j])
+			if i == len(letters)-1 {
+				fmt.Print("\n")
+			}
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("use one arg")
+	} else {
+		ascii_art()
+	}
+}
