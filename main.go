3ce40e2193bb180f4012c680a9d274c78a2b6113
morayod <morayodav@gmail.com>
2021-11-01 22:18:38 +0000

    changed comments

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func read_file(s string) []string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// replace newlines with space and newline and split on whitespaces
+	// replace newlines with space and random character and split using the random character
 	text := strings.Replace(string(content), "\n", " %", -1)
 	words := strings.FieldsFunc(text, func(r rune) bool { return strings.ContainsRune("%", r) })
 	return words
